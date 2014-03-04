d7390d0a7cca2a3ac815f1f8aaa2a6e825bad2a0
Joaquin <joaquin@isla.io>
2014-03-04 09:55:36 +0100

    Add onOptionsItemSelected handlers
    Add onOptionsItemSelected handlers
    
    Contact: joaquin@isla.io

--- app/src/main/java/com/example/myapplication2/app/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/app/MainActivity.java
@@ -19,10 +19,8 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
@@ -34,7 +32,12 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_search) {
+            openSearch();
+            return true;
+        }
+        if (id == R.id.action_settings){
+            openSettings();
             return true;
         }
         return super.onOptionsItemSelected(item);
